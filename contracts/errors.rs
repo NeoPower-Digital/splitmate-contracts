29b1d2fcf6fd3d68fe773ab4ee672279ce83ae78
pcorrado-np <pcorrado@neopower.digital>
2023-02-27 12:42:27 -0700

    SPL-15 feat: Added add_group message and main contract structure

---
+++ contracts/errors.rs
@@ -0,0 +1,3 @@
+#[derive(PartialEq, Debug, Eq, Clone, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum ContractError {}
---
+++ contracts/lib.rs
@@ -0,0 +1,99 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub mod errors;
+pub mod expense;
+pub mod group;
+
+#[ink::contract]
+mod splitmate {
+    use crate::errors::ContractError;
+    use crate::expense::Expense;
+    use crate::group::{Group, GroupMember};
+    use ink::prelude::vec::Vec;
+    use ink::storage::Mapping;
+
+    type BaseResult = Result<(), ContractError>;
+
+    #[ink(storage)]
+    pub struct Splitmate {
+        groups: Mapping<u128, Group>,                 // Group ID -> Group
+        expenses: Mapping<u128, Vec<Expense>>,        // Group ID -> Expenses
+        member_groups: Mapping<AccountId, Vec<u128>>, // Account -> Group IDs
+        next_group_id: u128,
+    }
+
+    impl Splitmate {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {
+                groups: Mapping::default(),
+                expenses: Mapping::default(),
+                member_groups: Mapping::default(),
+                next_group_id: 0,
+            }
+        }
+
+        #[ink(message)]
+        pub fn add_group(&mut self, members_to_add: Vec<AccountId>) -> BaseResult {
+            let next_group_id = self.next_group_id.clone();
+            let mut new_group_members = Vec::<GroupMember>::new();
+
+            for member in members_to_add {
+                let mut member_groups =
+                    self.member_groups.get(member).unwrap_or(Vec::<u128>::new());
+                member_groups.push(next_group_id);
+                self.member_groups.insert(member, &member_groups);
+
+                new_group_members.push(GroupMember {
+                    member,
+                    debt_value: 0,
+                });
+            }
+
+            let new_group = Group {
+                id: self.next_group_id.clone(),
+                members: new_group_members,
+            };
+
+            self.groups.insert(self.next_group_id.clone(), &new_group);
+            self.next_group_id = self.next_group_id.checked_add(1).unwrap();
+
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn add_expense(&mut self, group_id: u128, expense: Expense) -> BaseResult {
+            Ok(())
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use crate::expense::{Split, SplitType};
+        use ink::env::test::{
+            callee, default_accounts, set_account_balance, set_caller, DefaultAccounts,
+        };
+        use ink::env::DefaultEnvironment;
+
+        fn get_contract_account_id() -> AccountId {
+            callee::<DefaultEnvironment>()
+        }
+
+        fn get_default_accounts() -> DefaultAccounts<DefaultEnvironment> {
+            default_accounts::<DefaultEnvironment>()
+        }
+
+        fn set_sender(sender: AccountId) {
+            set_caller::<DefaultEnvironment>(sender);
+        }
+
+        fn set_balance(balance: u128) {
+            set_account_balance::<DefaultEnvironment>(get_contract_account_id(), balance)
+        }
+
+        fn init() -> (Splitmate, DefaultAccounts<DefaultEnvironment>) {
+            (Splitmate::new(), get_default_accounts())
+        }
+    }
+}
