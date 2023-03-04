9d35e3a969ab4db9f7dd7e00d5ac37973cdd5fce
pcorrado-np <pcorrado@neopower.digital>
2023-03-04 10:55:16 -0700

    SPL-22 SPL-23 feat: Added ERC20, distribute funds message and general refactor

--- contracts/splitmate/errors.rs
+++ contracts/splitmate/errors.rs
@@ -8,4 +8,5 @@ pub enum ContractError {
     ExpenseWithoutMembers,
     ExpenseWithoutDistributionMembers,
     GroupDoesNotExist,
+    TransferError,
 }
--- contracts/splitmate/output_models.rs
+++ contracts/splitmate/output_models.rs
@@ -16,3 +16,17 @@ pub struct DistributionMemberTransfer {
     pub member_account: AccountId,
     pub debt_value: u128,
 }
+
+#[derive(PartialEq, Debug, Eq, Clone, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo, StorageLayout))]
+pub struct SettledDebts {
+    pub group_id: u128,
+    pub receivers: Vec<AccountId>,
+}
+
+#[derive(PartialEq, Debug, Eq, Clone, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo, StorageLayout))]
+pub struct SettleUpResult {
+    pub result: bool,
+    pub settled_debts: Option<Vec<SettledDebts>>,
+}
